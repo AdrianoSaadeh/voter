32d5126bed20a99e2364debe9d84dbbef5ad5838
AdrianoSaadeh <34695232+AdrianoSaadeh@users.noreply.github.com>
2018-11-12 18:32:08 -0200

    Add alteracoes na VoterService

--- voter-master/src/main/java/br/edu/ulbra/election/voter/service/VoterService.java
+++ voter-master/src/main/java/br/edu/ulbra/election/voter/service/VoterService.java
@@ -14,104 +14,133 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Type;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 @Service
 public class VoterService {
 
-    private final VoterRepository voterRepository;
+	private final VoterRepository voterRepository;
 
-    private final ModelMapper modelMapper;
+	private final ModelMapper modelMapper;
 
-    private final PasswordEncoder passwordEncoder;
+	private final PasswordEncoder passwordEncoder;
 
-    private static final String MESSAGE_INVALID_ID = "Invalid id";
-    private static final String MESSAGE_VOTER_NOT_FOUND = "Voter not found";
+	private static final String MESSAGE_INVALID_ID = "Invalid id";
+	private static final String MESSAGE_VOTER_NOT_FOUND = "Voter not found";
 
-    @Autowired
-    public VoterService(VoterRepository voterRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder){
-        this.voterRepository = voterRepository;
-        this.modelMapper = modelMapper;
-        this.passwordEncoder = passwordEncoder;
-    }
+	@Autowired
+	public VoterService(VoterRepository voterRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
+		this.voterRepository = voterRepository;
+		this.modelMapper = modelMapper;
+		this.passwordEncoder = passwordEncoder;
+	}
 
-    public List<VoterOutput> getAll(){
-        Type voterOutputListType = new TypeToken<List<VoterOutput>>(){}.getType();
-        return modelMapper.map(voterRepository.findAll(), voterOutputListType);
-    }
+	public List<VoterOutput> getAll() {
+		Type voterOutputListType = new TypeToken<List<VoterOutput>>() {
+		}.getType();
+		return modelMapper.map(voterRepository.findAll(), voterOutputListType);
+	}
 
-    public VoterOutput create(VoterInput voterInput) {
-        validateInput(voterInput, false);
-        Voter voter = modelMapper.map(voterInput, Voter.class);
-        voter.setPassword(passwordEncoder.encode(voter.getPassword()));
-        voter = voterRepository.save(voter);
-        return modelMapper.map(voter, VoterOutput.class);
-    }
+	// ajuste para MD5
+	// public VoterOutput create(VoterInput voterInput) {
+	public VoterOutput create(VoterInput voterInput) throws NoSuchAlgorithmException {
+		validateInput(voterInput, false);
+		Voter voter = modelMapper.map(voterInput, Voter.class);
+		voter.setPassword(passwordEncoder.encode(voter.getPassword()));
+		voter = voterRepository.save(voter);
+		return modelMapper.map(voter, VoterOutput.class);
+	}
 
-    public VoterOutput getById(Long voterId){
-        if (voterId == null){
-            throw new GenericOutputException(MESSAGE_INVALID_ID);
-        }
+	public VoterOutput getById(Long voterId) {
+		if (voterId == null) {
+			throw new GenericOutputException(MESSAGE_INVALID_ID);
+		}
 
-        Voter voter = voterRepository.findById(voterId).orElse(null);
-        if (voter == null){
-            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
-        }
+		Voter voter = voterRepository.findById(voterId).orElse(null);
+		if (voter == null) {
+			throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
+		}
 
-        return modelMapper.map(voter, VoterOutput.class);
-    }
+		return modelMapper.map(voter, VoterOutput.class);
+	}
 
-    public VoterOutput update(Long voterId, VoterInput voterInput) {
-        if (voterId == null){
-            throw new GenericOutputException(MESSAGE_INVALID_ID);
-        }
-        validateInput(voterInput, true);
+	// ajuste para MD5
+	// public VoterOutput update(Long voterId, VoterInput voterInput) {
+	public VoterOutput update(Long voterId, VoterInput voterInput) throws NoSuchAlgorithmException {
 
-        Voter voter = voterRepository.findById(voterId).orElse(null);
-        if (voter == null){
-            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
-        }
+		if (voterId == null) {
+			throw new GenericOutputException(MESSAGE_INVALID_ID);
+		}
+		validateInput(voterInput, true);
 
-        voter.setEmail(voterInput.getEmail());
-        voter.setName(voterInput.getName());
-        if (!StringUtils.isBlank(voterInput.getPassword())) {
-            voter.setPassword(passwordEncoder.encode(voterInput.getPassword()));
-        }
-        voter = voterRepository.save(voter);
-        return modelMapper.map(voter, VoterOutput.class);
-    }
+		Voter voter = voterRepository.findById(voterId).orElse(null);
+		if (voter == null) {
+			throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
+		}
 
-    public GenericOutput delete(Long voterId) {
-        if (voterId == null){
-            throw new GenericOutputException(MESSAGE_INVALID_ID);
-        }
+		voter.setEmail(voterInput.getEmail());
+		voter.setName(voterInput.getName());
+		if (!StringUtils.isBlank(voterInput.getPassword())) {
+			voter.setPassword(passwordEncoder.encode(voterInput.getPassword()));
+		}
+		voter = voterRepository.save(voter);
+		return modelMapper.map(voter, VoterOutput.class);
+	}
 
-        Voter voter = voterRepository.findById(voterId).orElse(null);
-        if (voter == null){
-            throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
-        }
+	public GenericOutput delete(Long voterId) {
+		if (voterId == null) {
+			throw new GenericOutputException(MESSAGE_INVALID_ID);
+		}
 
-        voterRepository.delete(voter);
+		Voter voter = voterRepository.findById(voterId).orElse(null);
+		if (voter == null) {
+			throw new GenericOutputException(MESSAGE_VOTER_NOT_FOUND);
+		}
 
-        return new GenericOutput("Voter deleted");
-    }
+		voterRepository.delete(voter);
 
-    private void validateInput(VoterInput voterInput, boolean isUpdate){
-        if (StringUtils.isBlank(voterInput.getEmail())){
-            throw new GenericOutputException("Invalid email");
-        }
-        if (StringUtils.isBlank(voterInput.getName())){
-            throw new GenericOutputException("Invalid name");
-        }
-        if (!StringUtils.isBlank(voterInput.getPassword())){
-            if (!voterInput.getPassword().equals(voterInput.getPasswordConfirm())){
-                throw new GenericOutputException("Passwords doesn't match");
-            }
-        } else {
-            if (!isUpdate) {
-                throw new GenericOutputException("Password doesn't match");
-            }
-        }
-    }
+		return new GenericOutput("Voter deleted");
+	}
+
+	// ajuste para MD5 e para validar o email
+	// private void validateInput(VoterInput voterInput, boolean isUpdate){
+	private void validateInput(VoterInput voterInput, boolean isUpdate) throws NoSuchAlgorithmException {
+
+		if (StringUtils.isBlank(voterInput.getEmail()) || !validateEmail(voterInput.getEmail())) {
+			throw new GenericOutputException("Invalid email");
+		}
+		if (StringUtils.isBlank(voterInput.getName()) || voterInput.getName().length() < 5
+				|| voterInput.getName().split(" ").length < 2) {
+			throw new GenericOutputException("Invalid name");
+		}
+
+		// pegar nome e no mínimo 1 sobrenome
+		if (voterInput.getName().indexOf(" ") == -1) {
+			throw new GenericOutputException("Your name must contain a last name");
+		}
+
+		if (!StringUtils.isBlank(voterInput.getPassword())) {
+			if (!voterInput.getPassword().equals(voterInput.getPasswordConfirm())) {
+				throw new GenericOutputException("Passwords doesn't match");
+			}
+		} else {
+			if (!isUpdate) {
+				throw new GenericOutputException("Password doesn't match");
+			}
+		}
+	}
+
+	public boolean validateEmail(String email) {
+		Type partyOutputListType = new TypeToken<List<Voter>>() {
+		}.getType();
+
+		List<Voter> parties = modelMapper.map(voterRepository.findAll(), partyOutputListType);
+		for (Voter party : parties)
+			if (party.getEmail().equals(email))
+				return false;
+
+		return true;
+	}
 
 }
